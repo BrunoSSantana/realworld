3dcf771b14e8557418e2e95ef257cb76a266f3f2
BrunoSSantana <brunoosouza15@gmail.com>
2022-09-15 06:38:11 -0300

    refact: add validation to outsideRegisterFail

--- src/adapters/ports/db/db.ts
+++ src/adapters/ports/db/db.ts
@@ -7,6 +7,10 @@ import * as comment from '@/adapters/use-cases/article/add-comment-to-article-ad
 export const createUserInDB: user.OutsideRegisterUser = async (data) => {
   const registeredUser = await db.outsideRegisterUser(data)
 
+  if (!registeredUser) {
+    throw new Error('Internal error registering user!')
+  }
+
   const token = await jwt.generateToken({ id: registeredUser?.id })
 
   return {
